a3bdf2310569fc296537e3b6aabc341313442282
kermage <genealyson.torcende@gmail.com>
2025-04-21 23:25:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# WordPress Plugin Releaser
+
+> Push a tagged version to the official repository
+
+## Usage
+
+```yaml
+- uses: kermage/action-wp-plugin-releaser@latest
+
+  with:
+    # Custom plugin slug
+    # Default: ${GITHUB_REPOSITORY#*/}
+    slug: ''
+
+    # Path to the assets
+    # Default: .wporg
+    assets: ''
+
+    # Skip actual commit
+    # Default: false
+    dryrun: ''
+
+  env:
+    WPORG_USERNAME: ${{ secrets.WPORG_USERNAME }}
+    WPORG_PASSWORD: ${{ secrets.WPORG_PASSWORD }}
+```
---
+++ action.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+set -eu
+
+INPUT_SLUG="${INPUT_SLUG:-${GITHUB_REPOSITORY#*/}}"
+INPUT_ASSETS="${INPUT_ASSETS:-.wporg}"
+INPUT_DRYRUN="${INPUT_DRYRUN:-false}"
+VERSION="${GITHUB_REF##*/}"
+[[ "$VERSION" =~ ^v[0-9] ]] && VERSION="${VERSION#v}"
+SVN_URL="https://plugins.svn.wordpress.org/${INPUT_SLUG}"
+TAG_URL="${SVN_URL}/tags/${VERSION}"
+SVN_DIR="${RUNNER_TEMP}/${INPUT_SLUG}-svn"
+EXPORT_DIR="${RUNNER_TEMP}/${INPUT_SLUG}-export"
+
+
+echo "DRY RUN: $INPUT_DRYRUN"
+echo "SLUG:    $INPUT_SLUG"
+echo "ASSETS:  $INPUT_ASSETS"
+echo "VERSION: $VERSION"
+
+[[ -z "$WPORG_USERNAME" ]] && echo "WPORG_USERNAME variable is required." && exit 1
+[[ -z "$WPORG_PASSWORD" ]] && echo "WPORG_PASSWORD variable is required." && exit 1
+
+
+if ! curl -fsSL --head "$SVN_URL" > /dev/null 2>&1; then
+	echo "Invalid plugin!"
+	exit 1
+fi
+
+if curl -fsSL --head "$TAG_URL" > /dev/null 2>&1; then
+	echo "Already released!"
+	exit 1
+fi
+
+
+svn checkout --depth immediates "$SVN_URL" "$SVN_DIR"
+svn update --set-depth infinity "${SVN_DIR}/assets"
+svn update --set-depth infinity "${SVN_DIR}/trunk"
+
+
+git config safe.directory "$GITHUB_WORKSPACE"
+git config user.email "$GITHUB_ACTOR"
+git config user.name "$GITHUB_ACTOR"
+
+if [[ -n "$(git status --porcelain)" ]]; then
+	git add .
+	git commit -m "From previous step:"
+fi
+
+mkdir -p "$EXPORT_DIR"
+git archive HEAD | tar -xC "$EXPORT_DIR"
+rsync -rcz "${EXPORT_DIR}/" "${SVN_DIR}/trunk/" --delete
+
+if [[ -d "${GITHUB_WORKSPACE}/${INPUT_ASSETS}/" ]]; then
+	rsync -rcz "${GITHUB_WORKSPACE}/${INPUT_ASSETS}/" "${SVN_DIR}/assets/" --delete
+fi
+
+
+cd "$SVN_DIR"
+svn add . --force > /dev/null
+svn status | grep '^\!' | sed 's/! *//' | xargs -I% svn rm %@ > /dev/null
+svn cp "trunk" "tags/${VERSION}"
+
+if [[ -d "assets" ]]; then
+	file_types=("png" "jpg" "gif" "svg")
+	mime_types=("image/png" "image/jpeg" "image/gif" "image/svg+xml")
+
+	for i in "${!file_types[@]}"; do
+		if [ -n "$(find "assets" -maxdepth 1 -name "*.${file_types[$i]}" -print -quit)" ]; then
+			svn propset svn:mime-type "${mime_types[$i]}" "assets/"*."${file_types[$i]}" || true
+		fi
+	done
+fi
+
+svn update
+svn status
+
+if $INPUT_DRYRUN; then
+	exit
+fi
+
+if ! svn commit -m "Update to version $VERSION" --no-auth-cache --non-interactive  --username "$WPORG_USERNAME" --password "$WPORG_PASSWORD"; then
+	exit 1
+fi
